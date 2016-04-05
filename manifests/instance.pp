b026e70730b20375e3e52fb759ccf4fb1d54d565
Vincent Sellier <vsellier@exoplatform.com>
2016-04-05 10:33:19 +0200

    Manage port binding

--- manifests/instance.pp
+++ manifests/instance.pp
@@ -1,13 +1,16 @@
 define docker_elasticsearch::instance (
   $image              = 'elasticsearch',
   $version,
+  $net                = 'bridge',
   $data_dir,
   $node_name          = "${::fqdn}",
   $cluster_name       = 'elasticsearch',
   $publish_host       = "${::fqdn}",
+  $bind_address       = '127.0.0.1',
   $cluster_members    = undef,
-  $minimum_master     = undef,
-  $repos              = undef, 
+  $minimum_masters    = undef,
+  $repos              = undef,
+  $bind_ports         = true, 
   $rest_port          = 9200,
   $transport_port     = 9300,
   $heap_size          = '1g',
@@ -23,8 +26,12 @@ define docker_elasticsearch::instance (
   } ->
   docker::run { "elasticsearch-${name}" :
     image     => "${image}:${version}",
-    ports     => ["127.0.0.1:${rest_port}:9200", "127.0.0.1:${transport_port}:9300"],
+    ports     => $bind_ports ? {
+      true        => ["${bind_address}:${rest_port}:9200", "${bind_address}:${transport_port}:9300"],
+      default     => [],
+    },
     volumes   => ["${data_dir}:/usr/share/elasticsearch/data", "/etc/elasticsearch/${node_name}/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml"],
+    net       => "${net}",
   }
   
 }
